d1084a819c40f0ec942921247b0e75a64023a21e
Anastasia828 <aveatik827@gmail.com>
2024-08-25 23:29:52 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name='Nastya'
 print(name, type (name))
 age=35
 print(age, type(age))
-new_age=age+2
+new_age=(age+1)
 print(new_age, type(new_age))
 is_student=True
 print(is_student, type(is_student))
